540f921ab2916e9c5a419856076eef2e98005b51
Gus Dunn <w.gus.dunn@gmail.com>
2017-10-23 14:53:33 -0400

    travis ci setup

--- travis_pypi_setup.py
+++ travis_pypi_setup.py
@@ -20,7 +20,7 @@ except ImportError:
     from urllib.request import urlopen
 
 
-GITHUB_REPO = 'xguse/coordinator_data_tasks'
+GITHUB_REPO = 'ScottSnapperLab/coordinator_data_tasks'
 TRAVIS_CONFIG_FILE = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), '.travis.yml')
 
